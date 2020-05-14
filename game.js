270f27284eb950a18306070b530179368d9d3336
NithinSravan <56395258+NithinSravan@users.noreply.github.com>
2020-05-14 20:27:13 +0530

    Add files via upload

--- game.js
+++ game.js
@@ -1,16 +1,23 @@
 let canvas = document.getElementById("canvas");
 let ctx = canvas.getContext("2d");
-let ballX;
-let ballY;
-let ballRad;
 let count;
 let loop;
 let seg;
 let gameOver = 0;
 let audio = new Audio("jump_09.wav");
 let sound = new Audio("button.wav");
+let pick=new Audio("pickup.mp3")
 let song;
 let blast;
+let x1;
+let y1;
+let x2;
+let y2;
+let normVector;
+let dotProd;
+let dashX;
+let dashY;
+let collDis;
 let bestScore = document.getElementById('show');
 let s = 0;
 let max = -1;
@@ -23,8 +30,18 @@ let restart;
 let overlay = document.getElementById('overlay');
 let parts;
 let particles;
+let ball;
+let obs;
+let click;
+let sprites;
 let endsongs = ["dilwale.mp3", "eeee.mp3", "Astronomia.mp3"];
-
+window.onopen = function () {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    clearInterval(blast);
+    clearInterval(loop);//to stop setting multiple setIntervals on resize
+    setup();
+}
 window.onresize = function () {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
@@ -35,7 +52,21 @@ window.onresize = function () {
 window.onload = function () {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
+    clearInterval(blast);
+    clearInterval(loop);
     setup();
+}
+function distance(x1, y1, x2, y2) {
+    let dx = x1 - x2;
+    let dy = y1 - y2;
+    return (Math.sqrt((dx * dx) + (dy * dy)))
+}
+function norm(x1, y1, x2, y2){
+    let n={
+        x:(x2-x1)/distance(x1, y1, x2, y2),
+        y:(y2-y1)/distance(x1, y1, x2, y2)
+    }
+    return n;
 }
 function rand(min, max) {
     return (Math.random() * (max - min + 1) + min);
@@ -58,9 +89,9 @@ function explode() {
     }
     blast = setInterval(function () {
         ctx.clearRect(0, 0, innerWidth, innerHeight);
-        for (let i = 0; i < obs.length; i++) {
-            obs[i].draw();
-        }
+     //   for (let i = 0; i < obs.length; i++) {
+       //     obs[i].draw();
+        //}
         for (let i = 0; i < particles.length; i++) {
             particles[i].update();
         }
@@ -212,10 +243,10 @@ class Obstacle {
     endgame(start, end, portion, dir) {
         if (portion === 0 && dir === 0 || portion === 1 && dir === 1) {
             for (let j = 0; j < this.parts.length; j++) {
-    
+
                 if (this.parts[j].color !== ball.color) {
                     if ((Math.abs(this.parts[j].end % (Math.PI * 2)) > Math.abs(start) && Math.abs(this.parts[j].end % (Math.PI * 2)) < Math.abs(end))) {
-    
+                  
                         explode();
                     }
                 }
@@ -223,10 +254,10 @@ class Obstacle {
         }
         else if (portion === 0 && dir === 1 || portion === 1 && dir === 0) {
             for (let j = 0; j < this.parts.length; j++) {
-    
+
                 if (this.parts[j].color !== ball.color) {
                     if ((Math.abs(this.parts[j].start % (Math.PI * 2)) > Math.abs(start) && Math.abs(this.parts[j].start % (Math.PI * 2)) < Math.abs(end))) {
-    
+
                         explode();
                     }
                 }
@@ -234,11 +265,106 @@ class Obstacle {
         }
     };
 }
-let ball;
-let obs;
-let click;
+//polygon class inherits from obstacle and then use the angles to draw the lines of the polygon 
+class Polygon extends Obstacle{
+    constructor(x, y, radius, startAngle, endAngle, segments){
+        super(x,y,radius, startAngle, endAngle, segments);
+        this.parts;
+       
+    }
+    draw(){
+        var df = 0;
+        this.parts = new Array();
+
+        for (let i = 0; i < this.segments; i++) {
+
+            ctx.beginPath();
+            ctx.strokeStyle = color[i];
+            this.parts.push(new Part(this.startAngle + df, this.endAngle + df, color[i]));
+            ctx.lineCap="round";
+            ctx.moveTo(this.x+this.radius*Math.cos(this.startAngle+df),this.y+this.radius*Math.sin(this.startAngle+df));
+            ctx.lineTo(this.x+this.radius*Math.cos(this.endAngle+df),this.y+this.radius*Math.sin(this.endAngle+df))
+            ctx.lineWidth = canvas.height / 40;
+            ctx.stroke();
+            if (this.dir === 0) {
+                df += (2 * Math.PI / this.segments);
+            }
+            else {
+                df -= (2 * Math.PI / this.segments);
+            }
+
+        }
+    }
+    collide(){
+         
+        for(let i=0;i<this.parts.length;i++){
+            x1=this.x+this.radius*Math.cos(this.parts[i].start);
+            y1=this.y+this.radius*Math.sin(this.parts[i].start);
+            x2=this.x+this.radius*Math.cos(this.parts[i].end);
+            y2=this.y+this.radius*Math.sin(this.parts[i].end);
+            let d=distance(x1,y1,x2,y2);
+            unitVector=norm(x1,y1,x2,y2);
+            //dot product between the unitvector along the line and the vector joining centre of ball and one end
+            //this gives the projection of the vector joining centre of ball and one end on the line itself
+            dotProd=((ball.x-x1)*unitVector.x+(ball.y-y1)*unitVector.y);
+            //dashX and dashY are the coordinates of the shadow of the ball on the line or the porjection of the ball on the line
+            dashX=x1+(dotProd*(x2-x1)/d);
+            dashY=y1+(dotProd*(y2-y1)/d);
+            collDis=distance(dashX,dashY,ball.x,ball.y);
+            if(collDis<=ball.radius+canvas.height/80&&(distance(dashX,dashY,x1,y1)+distance(dashX,dashY,x2,y2)<=d)&&ball.color!==this.parts[i].color)
+            {   
+               explode()
+            }
+        }    
+    
+    };
+}
+//sprite class creates new sprite animations when constructed 
+class Sprite{
+    constructor(y){
+        this.cols = 4;
+        this.rows= 2;
+        this.maxF = this.cols * this.rows - 1;
+        this.currF = 0;
+        this.image = new Image();
+        this.image.src="balls.png";
+        this.frameWidth = this.image.width / this.cols;
+        this.frameHeight = this.image.height / this.rows;
+        this.y=y;
+        this.colorchange=0;
+    }
+    draw(){
+        let column = this.currF % this.cols; //cycles back to the start
+        let row = Math.floor(this.currF / this.cols); // row 1 : 0.....row 2: 1
+        ctx.drawImage(this.image, column * this.frameWidth, row * this.frameHeight, this.frameWidth, this.frameHeight, canvas.width/2-(canvas.height*0.02),this.y-(canvas.height*0.02) , canvas.height*0.045,  canvas.height*0.045);
+    }
+    update(i){
+        this.curtFrame++;
+        //reset and start from the first sprite
+        if (this.currF > this.maxF){
+            this.currF = 0;
+        }
+       this.y=obs[i].y-(3*canvas.height/10);
+        this.draw();
+        if(ball.y>=this.y-(canvas.height*0.025)&&ball.y<=(this.y+(canvas.height*0.025)))
+        {
+            //changes color only once on picking the power up
+            if(this.colorchange===0)
+            {
+                pick.play();
+                let j=Math.floor(rand(0,obs[i+1].parts.length-1));
+                ball.color=obs[i+1].parts[j].color;
+                this.image.src="";
+               this.colorchange++;
+            }
+           
+          
+        }
+    }
+}
+
 function setup() {
-    ball = new Ball(canvas.width / 2, (canvas.height / 2 + canvas.height * 0.3), canvas.height * 0.02, "#F5FF25");
+    ball = new Ball(canvas.width / 2, (canvas.height / 2 + canvas.height * 0.3), canvas.height * 0.015, "#F5FF25");
     s = 0;
     max = -1;
     gameOver = 0;
@@ -256,6 +382,8 @@ function setup() {
     click = 0;
     pause.addEventListener('click', pauseGame);
     obs = new Array();
+    sprites=new Array();
+    sprites.push(new Sprite((-canvas.height / 10)));
     createObs((canvas.width / 2), (canvas.height / 5), canvas.height * 0.15);
     score();
     play();
@@ -323,13 +451,13 @@ function play() {
 }
 //sets ball velocity based on the number of clicks. Provides a headstart if the game is started else sets a lower velocity for further clicks
 function setBallVel() {
-    audio.play();
+   audio.play();
     if (click === 0) {
-        ball.vy = Math.min(canvas.height * 0.015, 9.5);
+        ball.vy = Math.min(canvas.height * 0.015, 9);
         click++;
     }
     else
-        ball.vy = Math.min(canvas.height * 0.01, 5.5);
+        ball.vy = Math.min(canvas.height * 0.01, 4.25);
 }
 function best() {
     if (localStorage.getItem('high') === null)
@@ -358,8 +486,16 @@ function restartButton() {
     });
 }
 function createObs(x, y, r) {
-    seg = Math.floor(rand(2, 4));
-    obs.push(new Obstacle(x, y, r, 0, (2 * Math.PI / seg), seg));
+    let decide= Math.floor(rand(0, 1));
+   if(decide===0)
+    {
+        seg = Math.floor(rand(2, 4));
+        obs.push(new Obstacle(x, y, r, 0, (2 * Math.PI / seg), seg));
+    }
+    else{
+        seg = Math.floor(rand(3, 4));
+        obs.push(new Polygon(x, y, r, 0, (2 * Math.PI / seg), seg));
+    }
     count++;
     obs[count].draw();
 }
@@ -370,10 +506,12 @@ function gameUpdate() {
         ball.draw();
     for (let i = 0; i < obs.length; i++) {
         obs[i].update();
+        sprites[i].update(i);
         obs[i].collide();
         if (ball.y <= obs[i].y && ball.y >= obs[i].y - obs[i].radius && (i + 1) > max) {
             s = i + 1;
             max = s;
+            sprites.push(new Sprite(obs[i].y - (3 * canvas.height / 10)))
             createObs((canvas.width / 2), obs[i].y - (3 * canvas.height / 5), canvas.height * 0.15);
             obs[i + 1].angVel += (Math.PI) / 1000;
         }
